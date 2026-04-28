29c9e8d3b57d28641c8287b9ffe56ff76357c843
devgabrielsborges <dev.gabrielsborges@proton.me>
2026-04-27 22:28:16 -0300

    fix/fix wrong import

--- src/config/mlflow_init.py
+++ src/config/mlflow_init.py
@@ -1,6 +1,7 @@
 import os
 
 import mlflow
+import mlflow.tracking
 from dotenv import load_dotenv
 
 load_dotenv(override=True)
@@ -9,13 +10,16 @@ for token_var in ("AWS_SESSION_TOKEN", "AWS_SECURITY_TOKEN"):
     os.environ.pop(token_var, None)
 
 
-def init_mlflow():
+def init_mlflow(experiment_name=None):
     tracking_uri = os.getenv("MLFLOW_TRACKING_URI", "http://localhost:5000")
     mlflow.set_tracking_uri(tracking_uri)
 
-    competition = os.getenv("KAGGLE_COMPETITION_NAME", "default")
-    target = os.getenv("TARGET_COLUMN", "target")
-    experiment_name = f"{competition}/{target}"
+    if experiment_name is None:
+        competition = os.getenv("KAGGLE_COMPETITION_NAME", "default")
+        target = os.getenv("TARGET_COLUMN", "target")
+        experiment_name = f"{competition}/{target}"
 
     mlflow.set_experiment(experiment_name)
     mlflow.enable_system_metrics_logging()
+
+    return mlflow.tracking.MlflowClient()
